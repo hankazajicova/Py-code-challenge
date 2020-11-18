4c3762db0361e9f0cbf9cff5192f89dbb32405b8
zajiha <hanka.zajicova@gmail.com>
2020-11-18 23:06:24 +0100

    ex. 25 modification

--- src/pp_25_guessing_game_two.py
+++ src/pp_25_guessing_game_two.py
@@ -1,96 +1,87 @@
 import random
 
+user_possibilities = ["low", "high", "correct"]
 
-def input_validator(input_number: int) -> bool:
-    """
-    user guess validator
+def input_validator(user_input: str) -> bool:
+    """user input validation
 
     Args:
-        input_guess (int): user input integer
+        user_input (str): input string
 
     Returns:
-        bool: approval of the validation
+        bool: validation
     """
-    if 1 <= input_number <= 9:
+    if user_input in user_possibilities:
         return True
 
     return False
 
 
-def guess_the_number(input_number: int, guess_number: int) -> dict:
+# random approach
+def guess_two_random() -> None:
     """
-    Args:
-        input_number (int): user input number
-        guess_number (int): by programe guess number
-
-    Returns:
-        dict: validation
+    guess and user (secret) number validation
     """
-    if not input_validator(input_number):
-        print("You have to give me the number between 1 and 9 (included). Try again!")
-        return False
-
-    return evaluate_guess(input_number, guess_number)
-
-
-def evaluate_guess(user_input: int, guess_number: int) -> dict:
-    """
-    guess and input number evaluation
-
-    Args:
-        user_input (int): user input number
-        guess_number (int): by programme guessed number
-
-    Returns:
-        dict: dictionary with min and max value
-    """
-    min_num = 1
-    max_num = 9
-
-    if user_input == guess_number:
-        print("The number is {number}. You won!".format(number=user_input))
-        return {
-            'min' : min_num,
-            'max' : max_num
-            }
-
-    if guess_number < user_input:
-        min_num = guess_number + 1
-        print("Your guess {guess} is lower than the number. Guess again".format(guess=guess_number))
-    elif guess_number > user_input:
-        max_num = guess_number - 1
-        print("Your guess {guess} is higher than the number. Guess again".format(
-            guess=guess_number))
-
-    return {
-        'min' : min_num,
-        'max' : max_num
-        }
-
-
-def guess_game_two():
-    """
-    starter of the guess game two
-    """
-    user_input = int(input("Give me the number between 1 and 9 (included): "))
-    print(user_input)
+    min_value = 1
+    max_value = 100
     number_of_guesses = 0
-    min_num = 1
-    max_num = 9
     while True:
-        guess_number = random.randint(min_num, max_num)
-        print("Program guessed number: {guess_number}.".format(guess_number=guess_number))
-        game_state = guess_the_number(user_input, guess_number)
+        if (max_value - min_value) < 0:
+            print("This is not in expected range 1 - 100. Game over.")
+            return
+
+        prog_guess = random.randint(min_value, max_value)
+        print("My guess is : {prog_guess}".format(prog_guess = prog_guess))
         number_of_guesses += 1
+        user_validation = input("correct, low or high? ")
+        if not input_validator(user_validation):
+            print("please, input if it's correct, low or high")
+            continue
 
-        min_num = game_state['min']
-        max_num = game_state['max']
-        print("min is {min} and max is {max}".format(min=min_num, max=max_num))
+        if user_validation == "low":
+            min_value = prog_guess + 1
+        elif user_validation == "high":
+            max_value = prog_guess - 1
 
-        if game_state['min'] == 1 and game_state['max'] == 9:
+        if user_validation == "correct":
             break
 
-    print("Hey Program! You guessed {number} times. Enjoyed? Good. Try again.".format(
-        number=number_of_guesses))
+    print("Cool, your guess {guess} is correct. You guessed {number} times.".format(
+        guess=prog_guess, number=number_of_guesses))
 
-guess_game_two()
+guess_two_random()
+
+
+# faster approach
+def guess_two_half() -> None:
+    """
+    guess and user (secret) number validation
+    """
+    min_value = 1
+    max_value = 100
+    number_of_guesses = 0
+    while True:
+        if (max_value - min_value) < 0:
+            print("This is not in expected range 1 - 100. Game over.")
+            return
+
+        prog_guess = round((min_value + max_value)/2)
+        print("My guess is : {prog_guess}".format(prog_guess = prog_guess))
+        number_of_guesses += 1
+        user_validation = input("correct, low or high? ")
+        if not input_validator(user_validation):
+            print("please, input if it's correct, low or high")
+            continue
+
+        if user_validation == "low":
+            min_value = prog_guess
+        elif user_validation == "high":
+            max_value = prog_guess
+
+        if user_validation == "correct":
+            break
+
+    print("Cool, your guess {guess} is correct. You guessed {number} times.".format(
+        guess=prog_guess, number=number_of_guesses))
+
+guess_two_half()
